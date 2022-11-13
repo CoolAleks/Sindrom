ca0487eba105015ca2ffb3adcde651b657aa47ec
JaneEyre333 <nasgur@mail.ru>
2022-11-13 21:44:49 +0300

    JACOCO 100% coverage

--- src/main/java/ru/netology/SindromService.java
+++ src/main/java/ru/netology/SindromService.java
@@ -10,9 +10,22 @@ public class SindromService {
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
-        for (long income : incomes)
-            if (current_max < income)
+        for (long income : incomes) {
+            if (current_max <= income)
                 current_max = income;
+            current_max_index++;
+        }
         return current_max;
+    }
+
+    public long findMin(long[] incomes) {
+        long current_min_index = 0;
+        long current_min = incomes[0];
+        for (long income : incomes) {
+            if (current_min > income)
+                current_min = income;
+            current_min_index++;
+        }
+        return current_min;
     }
 }
--- src/test/java/ru/netology/SindromServiceTest.java
+++ src/test/java/ru/netology/SindromServiceTest.java
@@ -7,16 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class SindromServiceTest {
 
     @Test
-    void findMax() {
+    void shouldfindMax() {
         SindromService service = new SindromService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
+
+    @Test
+    void shouldFindMin() {
+        SindromService service = new SindromService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 3;
+
+        long actual = service.findMin(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
+
 
 
